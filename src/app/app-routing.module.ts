421995cf6bf6929f291ff0998aa54e08fb793325
zigiagozal <vita.vita8520@gmail.com>
2020-12-24 19:59:19 +0200

    vita potato|

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreatAcountComponent } from '../components/createacount/createacount.component';
+import { RulesComponent } from '../components/rules/rules.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'MyApp', component: AppComponent },
+  { path: 'CreatAcount', component: CreatAcountComponent },
+  { path: 'Rules', component: RulesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { RouterModule } from '@angular/router'
+//import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RulesComponent } from 'src/components/rules/rules.component';
+import { CreatAcountComponent } from 'src/components/createacount/createacount.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,RulesComponent,CreatAcountComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CreatAcountComponent, RulesComponent, AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
